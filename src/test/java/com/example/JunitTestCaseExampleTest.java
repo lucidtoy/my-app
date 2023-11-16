233ce7bf367ff7386354ea8caf46708b584d9ba5
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-16 19:27:41 +1030

    work on unit-test branch

--- src/test/java/com/example/JunitTestCaseExampleTest.java
+++ src/test/java/com/example/JunitTestCaseExampleTest.java
@@ -5,9 +5,33 @@ import static org.junit.Assert.assertEquals;
 
 public class JunitTestCaseExampleTest {
 
-    @Test
-    public void testAdd() {
-        JunitTestCaseExample example = new JunitTestCaseExample();
-        assertEquals(4, example.add(2, 2));
-    }
+	@Test
+    	public void testAdd() {
+        	JunitTestCaseExample example = new JunitTestCaseExample();
+        	assertEquals(4, example.add(2, 2));
+    	}
+
+	@Test
+    	public void testSubtract() {
+        	JunitTestCaseExample example = new JunitTestCaseExample();
+        	assertEquals(3, example.subtract(5, 2));
+    	}
+
+	@Test
+    	public void testMultiply() {
+        	JunitTestCaseExample example = new JunitTestCaseExample();
+        	assertEquals(10, example.multiply(5, 2));
+    	}
+
+	@Test
+    	public void testAdd_x() {
+        	JunitTestCaseExample example = new JunitTestCaseExample();
+        	assertEquals(10, example.add(5, 2));
+    	}
+	
+	@Test
+    	public void testAdd_x2() {
+        	JunitTestCaseExample example = new JunitTestCaseExample();
+        	assertEquals(8, example.add(5, 2));
+    	}
 }
