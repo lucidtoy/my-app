ff78570cfd89317cedfb2e75f9f81384e64ac2ed
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-16 18:38:14 +1030

    Modification of feature-y

--- src/main/java/com/example/JunitTestCaseExample.java
+++ src/main/java/com/example/JunitTestCaseExample.java
@@ -4,4 +4,13 @@ public class JunitTestCaseExample {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+
+    public int divide(int a, int b) {
+        return a - b;
+    }
 }
