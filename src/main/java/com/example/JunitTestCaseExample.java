db052f0ebd966eafb2a84a4071b8d0a25fa90506
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-16 18:57:32 +1030

    modification for feature-z

--- src/main/java/com/example/JunitTestCaseExample.java
+++ src/main/java/com/example/JunitTestCaseExample.java
@@ -9,10 +9,11 @@ public class JunitTestCaseExample {
         return a - b;
     }
 
-
-
-    public int divide(int a, int b) {
-        return a / b;
+    public int multiply(int a, int b) {
+        return a * b;
     }
 
+    public int divide(int a, int b) {
+        return a - b;
+    }
 }
